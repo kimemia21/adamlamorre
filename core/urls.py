b9f246fee4188db613b1a87c161dd8cbc1426c93
kimemia21 <bobbymbogo71@gmail.com>
2024-07-19 11:39:15 +0300

    working on responses

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import re_path
+from . import views
+
+urlpatterns = [
+    re_path("login",views.login),
+     re_path("signup",views.signup),
+      re_path("token",views.test_token)
+    
+]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from rest_framework import status
+from .serializer import UserSerializer
 
-# Create your views here.
+@api_view(["POST"])
+def login(request):
+    serializer =UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user =User.objects.get(username=request.data["username"])
+        token =Token.created(user=user)
+    return Response({"token":token.key,"user":serializer.data},status=status.HTTP_200_OK)
+
+@api_view(["POST"])
+def signup(request):
+    return Response({"message":"success"})
+
+@api_view(["GET"])
+def test_token(request):
+    return Response({"message":"success"})
